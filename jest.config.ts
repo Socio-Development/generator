a4fee87d59ca61e10c365a84bc325473f016af88
Casper Tollefsen <casper.tollefsen@telenor.no>
2023-12-08 14:43:35 +0100

    Update coverage thresholds in jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -46,22 +46,19 @@ export default {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 50,
-      functions: 50,
-      lines: 50,
-      statements: 50,
+      branches: 25,
+      functions: 33,
+      lines: 18,
     },
     './src/config.ts': {
       branches: 100,
       functions: 100,
       lines: 100,
-      statements: 100,
     },
     './src/utils.ts': {
       branches: 100,
       functions: 100,
       lines: 100,
-      statements: 100,
     },
   },
 
