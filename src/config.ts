1f00a4a5c21f175084cbe89d98731741636150b7
Casper Tollefsen <casper.tollefsen@telenor.no>
2023-12-08 14:34:28 +0100

    Add directory and file whitelists to config

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,18 @@ import defaultConfig from './defaultConfig'
 import { Config, UserConfig } from './types'
 
 /**
+ * A list of directory names that starts with a period (.github, .vscode).
+ * This list is used to help differentiate directories from files.
+ */
+export const directoryWhitelist = ['.github', '.husky', '.vscode']
+
+/**
+ * A list of file names that starts with a period (.gitignore, .prettierignore, .prettierrc).
+ * This list is used to help differentiate files from directories.
+ */
+export const fileWhitelist = ['.gitignore', '.prettierignore', '.prettierrc']
+
+/**
  * Creates a config object from the user provided config.
  * @param config The user provided config.
  * @returns A config object.
