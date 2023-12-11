e5c512e84cccf2dc0661a153019a1d1654560f3e
Casper Tollefsen <casper.tollefsen@telenor.no>
2023-12-11 11:23:26 +0100

    Add user config validation function

--- src/__tests__/config.test.ts
+++ src/__tests__/config.test.ts
@@ -1,4 +1,4 @@
-import { defineConfig, mergeConfig } from '../config'
+import { defineConfig, mergeConfig, validateUserConfig } from '../config'
 import { defaultConfig } from '../constants'
 import { UserConfig } from '../types'
 
@@ -34,6 +34,153 @@ describe('mergeConfig', () => {
       ...defaultConfig,
       ...userConfig,
       origin: 'user',
+    })
+  })
+})
+
+describe('validateUserConfig', () => {
+  it('should throw an error if the provided config is not an object', () => {
+    const userConfig = 'test'
+    expect(() => validateUserConfig(userConfig)).toThrow(
+      '[generator] User config must be an object. Make sure you are using `defineConfig()` to define your config.',
+    )
+  })
+
+  it('should throw an error if the provided config is null', () => {
+    const userConfig = null
+    expect(() => validateUserConfig(userConfig)).toThrow(
+      '[generator] User config must be an object. Make sure you are using `defineConfig()` to define your config.',
+    )
+  })
+
+  it('should throw an error if the provided config is undefined', () => {
+    const userConfig = undefined
+    expect(() => validateUserConfig(userConfig)).toThrow(
+      '[generator] User config must be an object. Make sure you are using `defineConfig()` to define your config.',
+    )
+  })
+
+  it('should return the provided config if it is an object', () => {
+    const userConfig = {}
+    const config = validateUserConfig(userConfig)
+    expect(config).toStrictEqual(userConfig)
+  })
+
+  it('should throw an error if the provided config has too many properties', () => {
+    const userConfig = {
+      test: true,
+      test2: true,
+      test3: true,
+      test4: true,
+      test5: true,
+      test6: true,
+      test7: true,
+      test8: true,
+      test9: true,
+    }
+    expect(() => validateUserConfig(userConfig)).toThrow(
+      '[generator] User config has too many properties.',
+    )
+  })
+
+  it('should throw an error if the provided config has invalid properties', () => {
+    const userConfig = {
+      test: true,
+    }
+    expect(() => validateUserConfig(userConfig)).toThrow(
+      '[generator] User config has invalid properties: test.',
+    )
+  })
+
+  describe('.createDir', () => {
+    it('should throw an error if .createDir is not a boolean', () => {
+      const userConfig = {
+        createDir: 'test',
+      }
+      expect(() => validateUserConfig(userConfig)).toThrow(
+        '[generator] User config property `createDir` must be a boolean.',
+      )
+    })
+  })
+
+  describe('.dotPrefixWhitelist', () => {
+    it('should throw an error if .dotPrefixWhitelist is not an object', () => {
+      const userConfig = {
+        dotPrefixWhitelist: 'test',
+      }
+      expect(() => validateUserConfig(userConfig)).toThrow(
+        '[generator] User config property `dotPrefixWhitelist` must be an object.',
+      )
+    })
+
+    describe('.dirs', () => {
+      it('should throw an error if .dotPrefixWhitelist.dirs contains is not an array', () => {
+        const userConfig = {
+          dotPrefixWhitelist: {
+            dirs: 'test',
+          },
+        }
+        expect(() => validateUserConfig(userConfig)).toThrow(
+          '[generator] User config property `dotPrefixWhitelist.dirs` must be an array.',
+        )
+      })
+
+      it('should throw an error if .dotPrefixWhitelist.dirs does not contain strings', () => {
+        const userConfig = {
+          dotPrefixWhitelist: {
+            dirs: [true],
+          },
+        }
+        expect(() => validateUserConfig(userConfig)).toThrow(
+          '[generator] User config property `dotPrefixWhitelist.dirs` must only contain strings.',
+        )
+      })
+    })
+
+    describe('.files', () => {
+      it('should throw an error if .dotPrefixWhitelist.files contains is not an array', () => {
+        const userConfig = {
+          dotPrefixWhitelist: {
+            files: 'test',
+          },
+        }
+        expect(() => validateUserConfig(userConfig)).toThrow(
+          '[generator] User config property `dotPrefixWhitelist.files` must be an array.',
+        )
+      })
+
+      it('should throw an error if .dotPrefixWhitelist.files does not contain strings', () => {
+        const userConfig = {
+          dotPrefixWhitelist: {
+            files: [true],
+          },
+        }
+        expect(() => validateUserConfig(userConfig)).toThrow(
+          '[generator] User config property `dotPrefixWhitelist.files` must only contain strings.',
+        )
+      })
+    })
+  })
+
+  describe('.safeMode', () => {
+    it('should throw an error if .safeMode is not a boolean', () => {
+      const userConfig = {
+        safeMode: 'test',
+      }
+      expect(() => validateUserConfig(userConfig)).toThrow(
+        '[generator] User config property `safeMode` must be a boolean.',
+      )
+    })
+  })
+
+  describe('.safetyDirName', () => {
+    it('should throw an error if .safetyDirName is not a string', () => {
+      const userConfig = {
+        safetyDirName: true,
+      }
+      expect(() => validateUserConfig(userConfig)).toThrow(
+        '[generator] User config property `safetyDirName` must be a string.',
+      )
     })
   })
 })
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 import { defaultConfig } from './constants'
 import { Config, ConfigOrigin, UserConfig } from './types'
+import { isObject } from './utils'
 
 /**
  * Creates a config object from the user provided config.
@@ -46,4 +47,131 @@ export function mergeConfig(userConfig: UserConfig): Config {
     },
     origin,
   }
+}
+
+/**
+ * Validates the user provided config.
+ * @param value The user provided config.
+ * @returns A config object.
+ * @throws An error if the user config is not an object.
+ * @throws An error if the user config has too many keys.
+ * @throws An error if the user config has invalid keys.
+ * @throws An error if the user config property `createDir` is not a boolean.
+ * @throws An error if the user config property `dotPrefixWhitelist` is not an object.
+ * @throws An error if the user config property `dotPrefixWhitelist.dirs` is not an array.
+ * @throws An error if the user config property `dotPrefixWhitelist.dirs` contains non-string values.
+ * @throws An error if the user config property `dotPrefixWhitelist.files` is not an array.
+ * @throws An error if the user config property `dotPrefixWhitelist.files` contains non-string values.
+ * @throws An error if the user config property `safeMode` is not a boolean.
+ * @throws An error if the user config property `safetyDirName` is not a string.
+ */
+export function validateUserConfig(value: unknown): UserConfig {
+  // check if the user config is an object
+  if (!isObject(value)) {
+    throw new Error(
+      '[generator] User config must be an object. Make sure you are using `defineConfig()` to define your config.',
+    )
+  }
+
+  const keysInUserConfig = Object.keys(value)
+
+  // check if the user config is empty
+  if (keysInUserConfig.length === 0) {
+    return {}
+  }
+
+  const keysInDefaultConfig = Object.keys(defaultConfig)
+
+  // check if user config has too many keys
+  if (keysInUserConfig.length > keysInDefaultConfig.length) {
+    throw new Error('[generator] User config has too many properties.')
+  }
+
+  // check if user config has invalid keys
+  const invalidKeys = keysInUserConfig.filter(
+    (key) => !keysInDefaultConfig.includes(key),
+  )
+  if (invalidKeys.length > 0) {
+    throw new Error(
+      `[generator] User config has invalid properties: ${invalidKeys.join(
+        ', ',
+      )}.`,
+    )
+  }
+
+  // validate .createDir
+  if (value.createDir) {
+    // check if .createDir is a boolean
+    if (typeof value.createDir !== 'boolean') {
+      throw new Error(
+        '[generator] User config property `createDir` must be a boolean.',
+      )
+    }
+  }
+
+  // validate .dotPrefixWhitelist
+  if (value.dotPrefixWhitelist) {
+    // check if .dotPrefixWhitelist is an object
+    if (!isObject(value.dotPrefixWhitelist)) {
+      throw new Error(
+        '[generator] User config property `dotPrefixWhitelist` must be an object.',
+      )
+    }
+    // validate .dotPrefixWhitelist.dirs
+    if (value.dotPrefixWhitelist.dirs) {
+      // check if .dotPrefixWhitelist.dirs is an array
+      if (!Array.isArray(value.dotPrefixWhitelist.dirs)) {
+        throw new Error(
+          '[generator] User config property `dotPrefixWhitelist.dirs` must be an array.',
+        )
+      }
+      // check if .dotPrefixWhitelist.dirs only contains strings
+      value.dotPrefixWhitelist.dirs.forEach((dir) => {
+        if (typeof dir !== 'string') {
+          throw new Error(
+            '[generator] User config property `dotPrefixWhitelist.dirs` must only contain strings.',
+          )
+        }
+      })
+    }
+    // validate .dotPrefixWhitelist.files
+    if (value.dotPrefixWhitelist.files) {
+      // check if .dotPrefixWhitelist.files is an array
+      if (!Array.isArray(value.dotPrefixWhitelist.files)) {
+        throw new Error(
+          '[generator] User config property `dotPrefixWhitelist.files` must be an array.',
+        )
+      }
+      // check if .dotPrefixWhitelist.files only contains strings
+      value.dotPrefixWhitelist.files.forEach((file) => {
+        if (typeof file !== 'string') {
+          throw new Error(
+            '[generator] User config property `dotPrefixWhitelist.files` must only contain strings.',
+          )
+        }
+      })
+    }
+  }
+
+  // validate .safeMode
+  if (value.safeMode) {
+    // check if .safeMode is a boolean
+    if (typeof value.safeMode !== 'boolean') {
+      throw new Error(
+        '[generator] User config property `safeMode` must be a boolean.',
+      )
+    }
+  }
+
+  // validate .safetyDirName
+  if (value.safetyDirName) {
+    // check if .safetyDirName is a string
+    if (typeof value.safetyDirName !== 'string') {
+      throw new Error(
+        '[generator] User config property `safetyDirName` must be a string.',
+      )
+    }
+  }
+
+  return value as UserConfig
 }
